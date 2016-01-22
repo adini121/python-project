e690bba840cf4536f2a91f3225b4adef147fd1b6
AdiMac <adamsken1221@gmail.com>
2016-01-22 13:50:23 +0100

    Printing full tables with all desired column names - before printing action-differences

--- src/MyParser.py
+++ src/MyParser.py
@@ -2,8 +2,11 @@ import re
 import subprocess
 import humanfriendly
 from humanfriendly.tables import format_pretty_table
-from humanfriendly.tables import format_smart_table
+from humanfriendly.tables import format_robust_table
 
+""" Filename: MyParser.py
+    What it does: For each pair of reference and comparable sessionId, parse and compare action files to output image differences and comparision result
+"""
 def extract_action_file_contents(sessionId, action_file_dir):
     try:
         file_content=open(action_file_dir + sessionId + '/' + 'actions.txt').read()
@@ -34,7 +37,7 @@ def get_the_action_id(file_contents):
     return reg_current_id
 
 IGNORE_ACTION = []
-def do_comparison(column_names, app_name, ref_sessionId_table_name, comp_sessionId_table_name,test_number,ref_sessionId, comp_sessionId,actions_in_reference_sessionId, actions_in_comp_sessionId, id_of_action_in_ref_sessionId_action_file, id_of_action_in_comp_sessionId_action_file):
+def do_comparison(app_name, ref_sessionId_table_name, comp_sessionId_table_name,test_number,ref_sessionId, comp_sessionId,actions_in_reference_sessionId, actions_in_comp_sessionId, id_of_action_in_ref_sessionId_action_file, id_of_action_in_comp_sessionId_action_file):
     """ do comparison """
     """ For printing:
     # print "the reference session has more # of actions. Reference SessionID ", ref_sessionID, "has # of actions:",len(actions_in_reference_sessionId)
@@ -42,34 +45,25 @@ def do_comparison(column_names, app_name, ref_sessionId_table_name, comp_session
     # print "number of actions in reference file are MORE than that of comparable file"
     """
     if len(actions_in_reference_sessionId) > len(actions_in_comp_sessionId):
-
         number_of_actions_in_reference_sessionId = range(len(actions_in_reference_sessionId))
         reference_sessionId_action = (actions_in_reference_sessionId, id_of_action_in_ref_sessionId_action_file)
         comp_sessionId_action = (actions_in_comp_sessionId, id_of_action_in_comp_sessionId_action_file)
-        # print  "More actions in Ref sessionId!"
     elif len(actions_in_reference_sessionId) == len(actions_in_comp_sessionId):
-        # print "number of actions in both files are EQUAL"
         number_of_actions_in_reference_sessionId = range(len(actions_in_reference_sessionId))
         reference_sessionId_action = (actions_in_reference_sessionId, id_of_action_in_ref_sessionId_action_file)
         comp_sessionId_action = (actions_in_comp_sessionId, id_of_action_in_comp_sessionId_action_file)
-        # print "Same number of actions!"
-
     else:
-        # print "Number of actions in reference file are LESS than that of comparable file"
         number_of_actions_in_reference_sessionId = range(len(actions_in_comp_sessionId))
         reference_sessionId_action = (actions_in_comp_sessionId, id_of_action_in_comp_sessionId_action_file)
         comp_sessionId_action = (actions_in_reference_sessionId, id_of_action_in_ref_sessionId_action_file)
-        # print "Less actions in Ref sessionId!"
 
-     # len(actions_in_reference_sessionId)
-    # print len(actions_in_reference_sessionId), len(actions_in_comp_sessionId)
+    num_of_ref_actions = '%d' % (len(actions_in_reference_sessionId))
+    num_of_comp_actions = '%d' % (len(actions_in_comp_sessionId))
 
     list = []
-    # humanfriendly_results=[[app_name,ref_sessionId_table_name[-5:], comp_sessionId_table_name[-5:],test_number, list[-1]]]
-
     for key in number_of_actions_in_reference_sessionId:
 
-        # """ Code to generate ImageDiff """
+        # """ Code to generate ImageDiff. Makes use of Imagemagick's convert (http://www.imagemagick.org/script/convert.php) """
         # num = '%d' % (test_number)
         # imgDir='/Users/adityanisal/ImageFiles/'
         # outDir='/Users/adityanisal/Desktop/ImageComparisonResults/' + app_name + '/'
@@ -99,18 +93,21 @@ def do_comparison(column_names, app_name, ref_sessionId_table_name, comp_session
         # """ Original logic without ignoring actions"""
 
         """ Code to generate action file differences """
+        ref_action_Id = id_of_action_in_ref_sessionId_action_file[key]
+        comp_action_Id = id_of_action_in_comp_sessionId_action_file[key]
+        # ref_action = []
+        # comp_action = []
         try:
             if not reference_sessionId_action[0][key] in IGNORE_ACTION:
                 assert reference_sessionId_action[0][key] == comp_sessionId_action[0][key]
+
                 list.append(0)
         except AssertionError:
-            # print "Mismatched"
-            # print "1st file id:", id_of_action_in_ref_sessionId_action_file[key]
-            # print "2nd file id:", id_of_action_in_comp_sessionId_action_file[key]
+            # ref_action = reference_sessionId_action[0][key]
+            # comp_action = comp_sessionId_action[0][key]
             list.append(1)
             break
         except KeyError:
-            # print "the action id %s can not be found" % reference_sessionId_action[1][key]
             list.append(1)
             break
 
@@ -119,7 +116,8 @@ def do_comparison(column_names, app_name, ref_sessionId_table_name, comp_session
     else:
         result= '0'
 
-    humanfriendly_results=[app_name,ref_sessionId_table_name[-5:], comp_sessionId_table_name[-5:],test_number, result]
+    humanfriendly_results=[test_number, result, num_of_ref_actions, num_of_comp_actions, ref_sessionId, comp_sessionId, ref_action_Id, comp_action_Id]
+    # action_differences = [ref_action, comp_action]
     return humanfriendly_results
 
 def get_all_data_of_session(sessionId, directory):
@@ -129,40 +127,37 @@ def get_all_data_of_session(sessionId, directory):
     return actions, action_id
 
 def main(ref_sessionId_table_name, comp_sessionId_table_name, action_files_dir, app_name):
-    # for row in range(len(tbl)):
+
     try:
         list_of_ref_sessionIds=connect_mysql.select_jenkins_1_580(ref_sessionId_table_name)
-        # print list_of_ref_sessionIds
-    except:
+    except IOError:
         print "ERROR : Reference table does not exist :", ref_sessionId_table_name
-    column_names = ['App Name','reference version', 'comparabale version','Test Number', 'Test Result']
 
-    # for row in range(len(tbl2)):
     try:
         list_of_comp_sessionIds=connect_mysql.select_jenkins_1_580(comp_sessionId_table_name)
-        # print  list_of_comp_sessionIds
-    except:
+    except IOError:
         print "ERROR : Comparable table does not exist :", comp_sessionId_table_name
+
+    column_names = ['Test Number','Test Result','Number of ref actions', 'Number of comp actions', 'ref sessionId', 'comp sessionId', 'ref action Id', 'comp action Id']
     final_list=[]
+
     try:
         if len(list_of_ref_sessionIds) == len(list_of_comp_sessionIds):
             rows_in_ref_sessionId_table = range(len(list_of_ref_sessionIds))
             for i in rows_in_ref_sessionId_table:
                 ref_action_file_data = get_all_data_of_session(list_of_ref_sessionIds[i], action_files_dir)
                 comp_action_file_data = get_all_data_of_session(list_of_comp_sessionIds[i], action_files_dir)
-                my_results=do_comparison(column_names,app_name, ref_sessionId_table_name, comp_sessionId_table_name,i,list_of_ref_sessionIds[i],list_of_comp_sessionIds[i],actions_in_reference_sessionId=ref_action_file_data[0], actions_in_comp_sessionId=comp_action_file_data[0], id_of_action_in_ref_sessionId_action_file=ref_action_file_data[1], id_of_action_in_comp_sessionId_action_file=comp_action_file_data[1])
+                # do_comparison(app_name, ref_sessionId_table_name, comp_sessionId_table_name,i,list_of_ref_sessionIds[i],list_of_comp_sessionIds[i],actions_in_reference_sessionId=ref_action_file_data[0], actions_in_comp_sessionId=comp_action_file_data[0], id_of_action_in_ref_sessionId_action_file=ref_action_file_data[1], id_of_action_in_comp_sessionId_action_file=comp_action_file_data[1])
+                my_results=do_comparison(app_name, ref_sessionId_table_name, comp_sessionId_table_name,i,list_of_ref_sessionIds[i],list_of_comp_sessionIds[i],actions_in_reference_sessionId=ref_action_file_data[0], actions_in_comp_sessionId=comp_action_file_data[0], id_of_action_in_ref_sessionId_action_file=ref_action_file_data[1], id_of_action_in_comp_sessionId_action_file=comp_action_file_data[1])
                 final_list.append(my_results)
-                # print final_list
-                # print "REF SID", list_of_ref_sessionIds[i]
-                # print "_____________________________________________________________________________________________________________________________________________________________"
-                # print "Test number:",i, "Reference_sessionID:",list_of_ref_sessionIds[i], "Comparable_sessionID:",list_of_comp_sessionIds[i]
-            """ Print results in a table """
-            print(format_pretty_table(final_list, column_names))
 
-        else:
-            print "reference sessionId table", ref_sessionId_table_name, "and comparable sessionId tables", comp_sessionId_table_name, "have different number of rows"
+
+            """ This command prints results in a table by making use of humanfriendly tabels (https://humanfriendly.readthedocs.org/en/latest/#module-humanfriendly.tables) """
+            print(format_pretty_table(final_list, column_names))
+        # else:
+            print "CAUTION! reference sessionId table: ", ref_sessionId_table_name, "and comparable sessionId table:", comp_sessionId_table_name, "Both have different number of rows"
     except:
-        print "TABLE ERROR: Please check if given reference table", ref_sessionId_table_name,  "and comparable table % exist", comp_sessionId_table_name
+        print "TABLE ERROR: Please check if given reference table", ref_sessionId_table_name,  "and comparable table exist", comp_sessionId_table_name
 
 
 
