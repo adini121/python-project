69f4beb492b486d0a9628c0b11a0e9a9f499a364
AdiMac <adamsken1221@gmail.com>
2016-02-21 08:53:02 +0100

    cleaning before merging phase2 files

--- src/comparator.py
+++
@@ -1,6 +0,0 @@
-from id_extractor import IDreader
-
-def reference_graph(self, reference_id_list, comparable_id_list, sessionId,):
-        self.reference_id_list = reference_id_list
-        self.comparable_id_list = comparable_id_list
-        self.sessionId = sessionId
